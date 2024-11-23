7bb8d562ac81f76ec723b94fc7c8677a323f6ca0
dirose <dirose@DESKTOP-PQA70RD>
2024-11-23 16:59:10 +0200

    Добавьте файлы проекта.

---
+++ laba3.4/Program.cs
@@ -0,0 +1,42 @@
+﻿double xStart = 0.5;
+double xEnd = 2.0;
+double h = 0.15;
+
+for (double x = xStart; x <= xEnd; x += h)
+{
+    double result = CalculateSeries(x);
+    Console.WriteLine($"f({x}) = {result}");
+}
+    
+double CalculateSeries(double x)
+{
+    double sum = 0.0;
+    int k = 1;
+    double previousTerm = 0.0;
+
+   
+    int maxTerms = 100;
+
+    for (k = 1; k <= maxTerms; k++)
+    {
+       
+        double term = (Math.Pow(1.5, k) * Math.Pow(x, k)) / Factorial(2 * k);
+
+        
+        if (Math.Abs(term) < 1e-10) break;
+
+        sum += term;
+    }
+
+    return sum;
+}
+
+double Factorial(int num)
+{
+    double factorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
